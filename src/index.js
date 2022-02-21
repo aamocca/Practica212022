7990e9f0c84a5668df3018913547832fdbd868e8
aamocca <aaroncafeina@hotmail.com>
2022-02-21 16:01:15 -0600

    Practica acabada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./assets/css/app.css";
+
+import App from "./components/App";
+import GenresInDb from "./components/GenresInDb";
+import LastMovieInDb from "./components/LastMovieInDb";
+import ContentRowMovies from "./components/ContentRowMovies";
+import SearchMovies from "./components/SearchMovies";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/GenresInDb" element={<GenresInDb />} />
+            <Route path="/LastMovieInDb" element={<LastMovieInDb />} />
+            <Route path="/ContentRowMovies" element={<ContentRowMovies />} />
+            <Route path="/Search" element={<SearchMovies />} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
